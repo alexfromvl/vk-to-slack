59bd165e6fe02a5b0bbcf2c9e408b5a327407fdd
Alex <alexromantsov@gmail.com>
2016-04-14 13:27:01 +1000

    Update vk-to-slack.py

--- vk-to-slack.py
+++ vk-to-slack.py
@@ -1,12 +1,13 @@
-from slacker import Slacker #импорт модуля для работы с API Slack
+from slacker import Slacker
 
-import vk_api #импорт модуля для работы с API Вконтакте
+import vk_api
 
-slack = Slacker('<API TOKEN>') #записываем токен от Slack bot
+import time
+
+slack = Slacker('Your token API Slack')
 
 def main():
-    
-    login, password = 'email@email.ru', 'pass' #авторизуемся в ВК
+    login, password = 'login', 'pass'
     vk_session = vk_api.VkApi(login, password)
 
     try:
@@ -15,12 +16,29 @@ def main():
         print(error_msg)
         return
 
-    vk = vk_session.get_api()
-    
-    response = vk.wall.getComments(owner_id=-1, post_id=1, count=1, extended=1)  # Используем метод wall.getComments для получения комментариев
-    print(response ['items'][0]['text']) #Получаем текст комментария со стены
 
-    slack.chat.post_message('#vk', 'Комментарий: ' + response ['items'][0]['text']) # Отправляем полученый комментарий в чат
+    vk = vk_session.get_api()
+
+    while True: #запускаем цикл
+        man_id = str(-29534144) #id группы с котороый будем брать посты и комментарии
+        postidlist = vk.wall.get(owner_id=man_id, count=1, offset=0) #получаем последний пост
+        a = str(postidlist['items'][0]['id']) #получаем id поста в виде цифры и записываем
+        time.sleep(5) #засыпаем на 5 секунд
+        ts = 10
+        bts = 10
+        while ts == bts: #сравниваем два числа времени комментариев, цикл выполняется до тех пор, пока цифры равны.
+            response = vk.wall.getComments(owner_id=man_id, post_id=a, count=1, sort='desc', offset=0) #Получаем последний комментарий
+            ts = str(response['items'][0]['date']) #запрашиваем время комментария
+            time.sleep(5) #ожидание 5 секунд
+            response = vk.wall.getComments(owner_id=man_id, post_id=a, count=1, sort='desc', offset=0)
+            bts = str(response['items'][0]['date'])
+            print('новых комментов нет ' + ts + bts) #Если новых комментариев нет выводим сообщение и время
+            time.sleep(5) #если время у комментариев разное цикл прекращается
+        response = vk.wall.getComments(owner_id=man_id, post_id=a, count=1, sort='desc', offset=0)  # получаем комментарий со стены
+        b = response['items'][0]['text'] #вытаскиваем из него только текст
+        print(b)
+        slack.chat.post_message('#vk', 'Новый комментарий: ' + b) #отправляем сообщение с текстом в Slack и повторяем цикл
 
 if __name__ == '__main__':
     main()
+
