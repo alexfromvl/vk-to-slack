cf92d6318e75c62a7354aff08092e1465f0553e5
Alex <alexromantsov@gmail.com>
2016-10-06 12:52:02 +1000

    Update vk-to-slack.py

--- vk-to-slack.py
+++ vk-to-slack.py
@@ -1,13 +1,14 @@
 from slacker import Slacker
-
 import vk_api
-
 import time
 
 slack = Slacker('Your token API Slack')
+login_vk = 'your login VK'
+pass_vk = 'your pass VK'
+group_id = '-1234567890'
 
 def main():
-    login, password = 'login', 'pass'
+    login, password = login_vk, pass_vk
     vk_session = vk_api.VkApi(login, password)
 
     try:
@@ -16,11 +17,10 @@ def main():
         print(error_msg)
         return
 
-
     vk = vk_session.get_api()
 
     while True: #запускаем цикл
-        man_id = str(-29534144) #id группы с котороый будем брать посты и комментарии
+        man_id = str(group_id) #id группы с которой будем брать посты и комментарии
         postidlist = vk.wall.get(owner_id=man_id, count=1, offset=0) #получаем последний пост
         a = str(postidlist['items'][0]['id']) #получаем id поста в виде цифры и записываем
         time.sleep(5) #засыпаем на 5 секунд
@@ -32,7 +32,7 @@ def main():
             time.sleep(5) #ожидание 5 секунд
             response = vk.wall.getComments(owner_id=man_id, post_id=a, count=1, sort='desc', offset=0)
             bts = str(response['items'][0]['date'])
-            print('новых комментов нет ' + ts + bts) #Если новых комментариев нет выводим сообщение и время
+            print('новых комментариев нет ' + ts + bts) #Если новых комментариев нет выводим сообщение и время
             time.sleep(5) #если время у комментариев разное цикл прекращается
         response = vk.wall.getComments(owner_id=man_id, post_id=a, count=1, sort='desc', offset=0)  # получаем комментарий со стены
         b = response['items'][0]['text'] #вытаскиваем из него только текст
